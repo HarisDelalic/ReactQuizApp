c83056cac05486ac943b9c390a82a86e054ebeec
haris_delalic <delalicharis@gmail.com>
2025-11-30 00:13:40 +0100

    add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import logo from "../assets/quiz-logo.png"
+export default function Header() {
+    return <header>
+        <img src = {logo}  alt="logo"/>
+        <h1>My Quiz App</h1>
+    </header>
+}
+

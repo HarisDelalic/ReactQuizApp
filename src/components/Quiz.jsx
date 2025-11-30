8e852f87023e195860038e5006bf0ab6538143dd
haris_delalic <delalicharis@gmail.com>
2025-12-01 00:09:45 +0100

    list questions and their shuffled answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,3 +1,44 @@
+import {useState} from "react";
+
+import QUESTIONS from "../questions"
+
+import quizCompleted from "../assets/quiz-complete.png"
+
 export default function Quiz() {
 
+    const [userAnswers, setUserAnswers] = useState([])
+
+    const activeQuestionIndex = userAnswers.length;
+
+    const isQuizCompleted = activeQuestionIndex === QUESTIONS.length
+
+    const handleSelectedAnswer = function (selectedAnswer) {
+        setUserAnswers(prevState => {
+           return [...prevState, selectedAnswer]
+        })
+    }
+
+    if (isQuizCompleted) {
+        return <div id="summary">
+            <img src={quizCompleted} alt="Quiz is completed"/>
+            <h2>Quiz is completed. Congratulations!</h2>
+        </div>
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
+    shuffledAnswers.sort(() => Math.random() - 0.5)
+
+    return (<div id="quiz">
+        <div id="question">
+            <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+
+            <ul id="answers">
+                {shuffledAnswers.map((answer) => (
+                    <li key={answer} className="answer">
+                        <button onClick={() => handleSelectedAnswer(answer)}>{answer}</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    </div>)
 }
